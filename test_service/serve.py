387db03f40acfa0040e11f7d1be044cf35217ba7
Bongani Sibanda <sibandabongz@gmail.com>
2018-03-18 20:42:21 +0200

    covert endpoint from http to use rpc

--- test_service/serve.py
+++ test_service/serve.py
@@ -1,20 +1,22 @@
 import json
 import lzma
-from nameko.web.handlers import http
+
+from nameko.rpc import rpc
 
 
 class TestService(object):
     name = 'invictus_service'
 
-    @http('GET', '/odd-number-squared')
-    def get_odd_number_squared(self, request):
-        list_of_integers = [1, 2, 3, 4, 5, 9]
+    @rpc
+    def get_odd_number_squared(self, list_of_integers):
         return json.dumps([pow(num, 2) if num & 1 else num for num in list_of_integers])
 
-    @http('GET', '/compressed-strings')
-    def get_compressed_strings(self, request):
-        list_of_strings = ['Bongani', 'Bernard', 'Sibanda']
-        return {string: lzma.compress(string.encode(encoding='utf-8')) for string in list_of_strings}
+    @rpc
+    def get_compressed_strings(self, list_of_strings):
+        return {string: lzma.compress(string.encode(encoding='utf-8')).decode(encoding='ISO-8859-1')
+                for string in list_of_strings}
 
-
+    @rpc
+    def get_decompressed_string(self, lzma_compressed_string):
+        return lzma.decompress(lzma_compressed_string.encode(encoding='ISO-8859-1')).decode(encoding='utf-8')
 
