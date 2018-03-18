62cf29c884c2d41daba23d393df2302be09e4e1f
Bongani Sibanda <sibandabongz@gmail.com>
2018-03-19 00:11:26 +0200

    use zlib for Huffman encoding

--- test_service/serve.py
+++ test_service/serve.py
@@ -1,5 +1,4 @@
-import json
-import lzma
+import zlib
 
 from nameko.rpc import rpc
 
@@ -9,14 +8,13 @@ class TestService(object):
 
     @rpc
     def get_odd_number_squared(self, list_of_integers):
-        return json.dumps([pow(num, 2) if num & 1 else num for num in list_of_integers])
+        return [pow(num, 2) if num & 1 else num for num in list_of_integers]
 
     @rpc
     def get_compressed_strings(self, list_of_strings):
-        return {string: lzma.compress(string.encode(encoding='utf-8')).decode(encoding='ISO-8859-1')
+        return {string: zlib.compress(string.encode(encoding='utf-8')).decode(encoding='ISO-8859-1')
                 for string in list_of_strings}
 
     @rpc
-    def get_decompressed_string(self, lzma_compressed_string):
-        return lzma.decompress(lzma_compressed_string.encode(encoding='ISO-8859-1')).decode(encoding='utf-8')
-
+    def get_decompressed_string(self, zlib_compressed_string):
+        return zlib.decompress(zlib_compressed_string.encode(encoding='ISO-8859-1')).decode(encoding='utf-8')
